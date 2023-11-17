6fba5cb66e5ac82f19013a16e285a67c9fadb730
Bryan Richardi <richardi_bryan@outlook.com>
2023-11-17 16:19:35 -0500

    correciones de letras

--- src/Paginas/Inicio.jsx
+++ src/Paginas/Inicio.jsx
@@ -10,18 +10,18 @@ export function Inicio () {
                         <img src= {imagen1} alt="IMAGEN1" />
                     </div>
                     <div className="sct1texto">
-                        <h1>Administración de Edicios y Seguridad</h1>
+                        <h1>Administración de Edificios y Seguridad</h1>
                         <p>
                         Ofrecemos una amplia gama de servicios generales para 
                         satisfacer todas sus necesidades de mantenimiento y reparación
                         </p>
-                        <button>Solicitar información</button>
+                        <button className='btn-solicitar'>Solicitar información</button>
                     </div>
                 </section>
 
                 <section id="sct2">
                     <h2>Los mejores en Administración de 
-                        <span> Edficios y seguridad</span>
+                        <span> Edificios y seguridad</span>
                     </h2>
                 </section>
                 <section id="sct3"></section>
