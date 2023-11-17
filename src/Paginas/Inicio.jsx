7d66dbfc741d322866893f3677367cf97b68cc0d
Bryan Richardi <richardi_bryan@outlook.com>
2023-11-17 15:47:24 -0500

    Avance pg principal

--- src/Paginas/Inicio.jsx
+++ src/Paginas/Inicio.jsx
@@ -1,70 +1,32 @@
-
+import imagen1 from '../Img/bgPrincipal.png'
+import '../Css/Container.css'
 
 export function Inicio () {
     return (
         <>
-        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quas illo repellat quo harum voluptates officiis dolorem. Sunt, rerum cum eveniet suscipit facere provident adipisci similique libero animi omnis rem! Assumenda.</p>
-        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quas illo repellat quo harum voluptates officiis dolorem. Sunt, rerum cum eveniet suscipit facere provident adipisci similique libero animi omnis rem! Assumenda.</p>
-        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quas illo repellat quo harum voluptates officiis dolorem. Sunt, rerum cum eveniet suscipit facere provident adipisci similique libero animi omnis rem! Assumenda.</p>
-        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quas illo repellat quo harum voluptates officiis dolorem. Sunt, rerum cum eveniet suscipit facere provident adipisci similique libero animi omnis rem! Assumenda.</p>
-        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quas illo repellat quo harum voluptates officiis dolorem. Sunt, rerum cum eveniet suscipit facere provident adipisci similique libero animi omnis rem! Assumenda.</p>
-        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quas illo repellat quo harum voluptates officiis dolorem. Sunt, rerum cum eveniet suscipit facere provident adipisci similique libero animi omnis rem! Assumenda.</p>
-        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quas illo repellat quo harum voluptates officiis dolorem. Sunt, rerum cum eveniet suscipit facere provident adipisci similique libero animi omnis rem! Assumenda.</p>
-        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quas illo repellat quo harum voluptates officiis dolorem. Sunt, rerum cum eveniet suscipit facere provident adipisci similique libero animi omnis rem! Assumenda.</p>
-        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quas illo repellat quo harum voluptates officiis dolorem. Sunt, rerum cum eveniet suscipit facere provident adipisci similique libero animi omnis rem! Assumenda.</p>
-        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quas illo repellat quo harum voluptates officiis dolorem. Sunt, rerum cum eveniet suscipit facere provident adipisci similique libero animi omnis rem! Assumenda.</p>
-        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quas illo repellat quo harum voluptates officiis dolorem. Sunt, rerum cum eveniet suscipit facere provident adipisci similique libero animi omnis rem! Assumenda.</p>
-        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quas illo repellat quo harum voluptates officiis dolorem. Sunt, rerum cum eveniet suscipit facere provident adipisci similique libero animi omnis rem! Assumenda.</p>
-        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quas illo repellat quo harum voluptates officiis dolorem. Sunt, rerum cum eveniet suscipit facere provident adipisci similique libero animi omnis rem! Assumenda.</p>
-        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quas illo repellat quo harum voluptates officiis dolorem. Sunt, rerum cum eveniet suscipit facere provident adipisci similique libero animi omnis rem! Assumenda.</p>
-        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quas illo repellat quo harum voluptates officiis dolorem. Sunt, rerum cum eveniet suscipit facere provident adipisci similique libero animi omnis rem! Assumenda.</p>
-        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quas illo repellat quo harum voluptates officiis dolorem. Sunt, rerum cum eveniet suscipit facere provident adipisci similique libero animi omnis rem! Assumenda.</p>
-        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quas illo repellat quo harum voluptates officiis dolorem. Sunt, rerum cum eveniet suscipit facere provident adipisci similique libero animi omnis rem! Assumenda.</p>
-        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quas illo repellat quo harum voluptates officiis dolorem. Sunt, rerum cum eveniet suscipit facere provident adipisci similique libero animi omnis rem! Assumenda.</p>
-        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quas illo repellat quo harum voluptates officiis dolorem. Sunt, rerum cum eveniet suscipit facere provident adipisci similique libero animi omnis rem! Assumenda.</p>
-        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quas illo repellat quo harum voluptates officiis dolorem. Sunt, rerum cum eveniet suscipit facere provident adipisci similique libero animi omnis rem! Assumenda.</p>
-        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quas illo repellat quo harum voluptates officiis dolorem. Sunt, rerum cum eveniet suscipit facere provident adipisci similique libero animi omnis rem! Assumenda.</p>
-        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quas illo repellat quo harum voluptates officiis dolorem. Sunt, rerum cum eveniet suscipit facere provident adipisci similique libero animi omnis rem! Assumenda.</p>
-        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quas illo repellat quo harum voluptates officiis dolorem. Sunt, rerum cum eveniet suscipit facere provident adipisci similique libero animi omnis rem! Assumenda.</p>
-        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quas illo repellat quo harum voluptates officiis dolorem. Sunt, rerum cum eveniet suscipit facere provident adipisci similique libero animi omnis rem! Assumenda.</p>
-        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quas illo repellat quo harum voluptates officiis dolorem. Sunt, rerum cum eveniet suscipit facere provident adipisci similique libero animi omnis rem! Assumenda.</p>
-        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quas illo repellat quo harum voluptates officiis dolorem. Sunt, rerum cum eveniet suscipit facere provident adipisci similique libero animi omnis rem! Assumenda.</p>
-        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quas illo repellat quo harum voluptates officiis dolorem. Sunt, rerum cum eveniet suscipit facere provident adipisci similique libero animi omnis rem! Assumenda.</p>
-        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quas illo repellat quo harum voluptates officiis dolorem. Sunt, rerum cum eveniet suscipit facere provident adipisci similique libero animi omnis rem! Assumenda.</p>
-        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quas illo repellat quo harum voluptates officiis dolorem. Sunt, rerum cum eveniet suscipit facere provident adipisci similique libero animi omnis rem! Assumenda.</p>
-        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quas illo repellat quo harum voluptates officiis dolorem. Sunt, rerum cum eveniet suscipit facere provident adipisci similique libero animi omnis rem! Assumenda.</p>
-        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quas illo repellat quo harum voluptates officiis dolorem. Sunt, rerum cum eveniet suscipit facere provident adipisci similique libero animi omnis rem! Assumenda.</p>
-        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quas illo repellat quo harum voluptates officiis dolorem. Sunt, rerum cum eveniet suscipit facere provident adipisci similique libero animi omnis rem! Assumenda.</p>
-        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quas illo repellat quo harum voluptates officiis dolorem. Sunt, rerum cum eveniet suscipit facere provident adipisci similique libero animi omnis rem! Assumenda.</p>
-        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quas illo repellat quo harum voluptates officiis dolorem. Sunt, rerum cum eveniet suscipit facere provident adipisci similique libero animi omnis rem! Assumenda.</p>
-        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quas illo repellat quo harum voluptates officiis dolorem. Sunt, rerum cum eveniet suscipit facere provident adipisci similique libero animi omnis rem! Assumenda.</p>
-        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quas illo repellat quo harum voluptates officiis dolorem. Sunt, rerum cum eveniet suscipit facere provident adipisci similique libero animi omnis rem! Assumenda.</p>
-        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quas illo repellat quo harum voluptates officiis dolorem. Sunt, rerum cum eveniet suscipit facere provident adipisci similique libero animi omnis rem! Assumenda.</p>
-        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quas illo repellat quo harum voluptates officiis dolorem. Sunt, rerum cum eveniet suscipit facere provident adipisci similique libero animi omnis rem! Assumenda.</p>
-        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quas illo repellat quo harum voluptates officiis dolorem. Sunt, rerum cum eveniet suscipit facere provident adipisci similique libero animi omnis rem! Assumenda.</p>
-        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quas illo repellat quo harum voluptates officiis dolorem. Sunt, rerum cum eveniet suscipit facere provident adipisci similique libero animi omnis rem! Assumenda.</p>
-        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quas illo repellat quo harum voluptates officiis dolorem. Sunt, rerum cum eveniet suscipit facere provident adipisci similique libero animi omnis rem! Assumenda.</p>
-        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quas illo repellat quo harum voluptates officiis dolorem. Sunt, rerum cum eveniet suscipit facere provident adipisci similique libero animi omnis rem! Assumenda.</p>
-        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quas illo repellat quo harum voluptates officiis dolorem. Sunt, rerum cum eveniet suscipit facere provident adipisci similique libero animi omnis rem! Assumenda.</p>
-        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quas illo repellat quo harum voluptates officiis dolorem. Sunt, rerum cum eveniet suscipit facere provident adipisci similique libero animi omnis rem! Assumenda.</p>
-        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quas illo repellat quo harum voluptates officiis dolorem. Sunt, rerum cum eveniet suscipit facere provident adipisci similique libero animi omnis rem! Assumenda.</p>
-        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quas illo repellat quo harum voluptates officiis dolorem. Sunt, rerum cum eveniet suscipit facere provident adipisci similique libero animi omnis rem! Assumenda.</p>
-        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quas illo repellat quo harum voluptates officiis dolorem. Sunt, rerum cum eveniet suscipit facere provident adipisci similique libero animi omnis rem! Assumenda.</p>
-        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quas illo repellat quo harum voluptates officiis dolorem. Sunt, rerum cum eveniet suscipit facere provident adipisci similique libero animi omnis rem! Assumenda.</p>
-        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quas illo repellat quo harum voluptates officiis dolorem. Sunt, rerum cum eveniet suscipit facere provident adipisci similique libero animi omnis rem! Assumenda.</p>
-        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quas illo repellat quo harum voluptates officiis dolorem. Sunt, rerum cum eveniet suscipit facere provident adipisci similique libero animi omnis rem! Assumenda.</p>
-        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quas illo repellat quo harum voluptates officiis dolorem. Sunt, rerum cum eveniet suscipit facere provident adipisci similique libero animi omnis rem! Assumenda.</p>
-        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quas illo repellat quo harum voluptates officiis dolorem. Sunt, rerum cum eveniet suscipit facere provident adipisci similique libero animi omnis rem! Assumenda.</p>
-        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quas illo repellat quo harum voluptates officiis dolorem. Sunt, rerum cum eveniet suscipit facere provident adipisci similique libero animi omnis rem! Assumenda.</p>
-        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quas illo repellat quo harum voluptates officiis dolorem. Sunt, rerum cum eveniet suscipit facere provident adipisci similique libero animi omnis rem! Assumenda.</p>
-        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quas illo repellat quo harum voluptates officiis dolorem. Sunt, rerum cum eveniet suscipit facere provident adipisci similique libero animi omnis rem! Assumenda.</p>
-        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quas illo repellat quo harum voluptates officiis dolorem. Sunt, rerum cum eveniet suscipit facere provident adipisci similique libero animi omnis rem! Assumenda.</p>
-        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quas illo repellat quo harum voluptates officiis dolorem. Sunt, rerum cum eveniet suscipit facere provident adipisci similique libero animi omnis rem! Assumenda.</p>
-        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quas illo repellat quo harum voluptates officiis dolorem. Sunt, rerum cum eveniet suscipit facere provident adipisci similique libero animi omnis rem! Assumenda.</p>
-        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quas illo repellat quo harum voluptates officiis dolorem. Sunt, rerum cum eveniet suscipit facere provident adipisci similique libero animi omnis rem! Assumenda.</p>
-        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quas illo repellat quo harum voluptates officiis dolorem. Sunt, rerum cum eveniet suscipit facere provident adipisci similique libero animi omnis rem! Assumenda.</p>
-        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quas illo repellat quo harum voluptates officiis dolorem. Sunt, rerum cum eveniet suscipit facere provident adipisci similique libero animi omnis rem! Assumenda.</p>
-    
+            <div id="inicio">
+                <section id="sct1">
+                    <div className='container-img'>
+                        <img src= {imagen1} alt="IMAGEN1" />
+                    </div>
+                    <div className="sct1texto">
+                        <h1>Administración de Edicios y Seguridad</h1>
+                        <p>
+                        Ofrecemos una amplia gama de servicios generales para 
+                        satisfacer todas sus necesidades de mantenimiento y reparación
+                        </p>
+                        <button>Solicitar información</button>
+                    </div>
+                </section>
+
+                <section id="sct2">
+                    <h2>Los mejores en Administración de 
+                        <span> Edficios y seguridad</span>
+                    </h2>
+                </section>
+                <section id="sct3"></section>
+                <section id="sct4"></section>
+            </div>
         </>
     )
 }
