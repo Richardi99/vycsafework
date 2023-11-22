c55fadb780d4b836ba3bd20e19fbee3cc4fcc0db
Richardi99 <richardi_bryan@outlook.com>
2023-11-22 02:34:05 -0500

    AVANCE DE SECICION NOSOTROS

--- src/Paginas/Nosotros.jsx
+++ src/Paginas/Nosotros.jsx
@@ -27,6 +27,48 @@ export const Nosotros = () => {
                     </div>
                 </div>
             </section>
+
+            <section id='valorSocial'>
+                <div className="card1">
+                    <h4 className='fraseSocial'>CALIDAD Y CONTINUO VALOR SOCIAL</h4>
+                    <p>Buscamos dar siempre el mejor servicio a nuestros clientes</p>
+                </div>
+
+                <div className="card2">
+                    <h4>Visión</h4>
+                        <p>
+                            Ser una corporación líder en el rubro de seguridad y vigilancia, Administración de 
+                            Edificios, destacándonos por ofrecer una gestión innovadora, profesional e integral 
+                            de excelencia. A su vez, generar un valor agregado al cliente, haciendo la diferencia 
+                            más allá de sus expectativas y logrando establecer relaciones a largo plazo.
+                        </p>
+                </div>
+                
+                
+                <div className="card4">
+                    <h4>Valores</h4>
+                        <p>
+                           <li>Transparencia</li>
+                           <li>Honestidad</li>
+                           <li>LibertadCoherencia como valor empresarial</li>
+                           <li>Puntualidad en los procesos</li>
+                           <li>Excelencia</li>
+                           <li>Adaptabilidad</li>
+                           <li>Diligencia</li>
+                        </p>
+                </div>
+
+                <div className="card3">
+                    <h4>Misión</h4>
+                        <p>
+                            Garantizar la prevención, seguridad, protección y administración de los bienes de nuestros
+                            clientes, brindándoles nuestros servicios personalizados, empleando soluciones integrales
+                            con implementación de tecnología innovadora, adaptándonos a las necesidades y exigencias
+                            requeridas. Logrando así, generar tranquilidad y confianza con el cuidado de sus 
+                            empresas
+                        </p>
+                </div>
+            </section>
         </div>
     )
 }
