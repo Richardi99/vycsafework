1d8a8daf1233f2f0db346db119f967aed50277da
Richardi99 <richardi_bryan@outlook.com>
2023-11-23 00:00:51 -0500

    actualizacion de img

--- src/Paginas/Servicios.jsx
+++ src/Paginas/Servicios.jsx
@@ -1,5 +1,19 @@
 import { Sct3Servicios } from '../Component/Sct3Servicios'
+import IMG1 from '../Img/IMG1.jpg'
+import IMG2 from '../Img/IMG2.jpg'
+import IMG3 from '../Img/IMG3.jpg'
+import IMG4 from '../Img/IMG4.jpg'
+import IMG5 from '../Img/IMG5.jpg'
+import IMG6 from '../Img/IMG6.jpg'
+import IMG7 from '../Img/IMG7.jpg'
+import IMG8 from '../Img/IMG8.jpg'
+import IMG9 from '../Img/IMG9.jpg'
+import IMG10 from '../Img/IMG10.jpg'
+import IMG11 from '../Img/IMG11.jpg'
 
+
+import { Formulario } from '../Component/Formulario'
+import { Brochure } from '../Component/Brochure'
 
 export const Servicio = () => {
     return (
@@ -12,10 +26,10 @@ export const Servicio = () => {
                 <div className="servicio-container">
 
                     <div className="servicio-texto">
-                        <h3>Nuestros <strong>Servicios</strong></h3>
-                        <h4>Administración de Edificios y Seguridad</h4>
+                        <h3>Te <strong>Brindamos</strong></h3>
+                        
                             <p>
-                            Nuestros servicios destacan por su excelencia y compromiso con 
+                            Nuestros servicios, los cuales destacan por su excelencia y compromiso con 
                             la satisfacción del cliente. Ofrecemos soluciones innovadoras y
                             personalizadas, respaldadas por un equipo experto y orientado 
                             a brindar resultados superiores. Desde atención al cliente hasta
@@ -31,8 +45,24 @@ export const Servicio = () => {
             <Sct3Servicios/>
 
             <section id='otros-Servicios'>
-
+                <h3>Comprometidos con el trabajo</h3>
+                <div className="galeria-img">
+                    <img src= {IMG10} alt="IMG" />
+                    <img src= {IMG1} alt="IMG" />
+                    <img src= {IMG2} alt="IMG" />
+                    <img src= {IMG3} alt="IMG" />
+                    <img src= {IMG4} alt="IMG" />
+                    <img src= {IMG5} alt="IMG" />
+                    <img src= {IMG6} alt="IMG" />
+                    <img src= {IMG7} alt="IMG" />
+                    <img src= {IMG9} alt="IMG" />
+                    <img src= {IMG11} alt="IMG" />
+                    
+                </div>
             </section>
+
+            <Formulario mostrarTitulo={true }/>
+            <Brochure/>
         </div>
     )
 }
