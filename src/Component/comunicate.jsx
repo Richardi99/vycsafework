ef32e7ff0bc347a89b5524786a9400d401b38ecf
Richardi99 <richardi_bryan@outlook.com>
2024-02-08 10:14:42 -0500

    update numero cel

--- src/Component/comunicate.jsx
+++ src/Component/comunicate.jsx
@@ -2,7 +2,7 @@
 export const Comunicate = ({comunicate}) => {
     return (
         <div className="bar-contact">
-            { comunicate && <h3>Comunícate con nosotros al +51 903287955</h3>}
+            { comunicate && <h3>Comunícate con nosotros al +51 902387955</h3>}
         </div>
     )
 }
